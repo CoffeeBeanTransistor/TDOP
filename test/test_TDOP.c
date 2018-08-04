c414b978d236dc2b7118beec43e78451b3478043
Wong Yik Kai <yikkai1@hotmail.com>
2018-08-04 19:58:13 +0800

    Updated and fixed bugs for tests

--- test/test_TDOP.c
+++ test/test_TDOP.c
@@ -29,16 +29,16 @@ void test_getTokenInfo_given_binary_plus_sign_token_expect_binding_power_equals_
 void test_nudInt_given_1_should_return_1(void) {
   Tokenizer *expression;
   Token *token;
-  IntegerToken *intToken;
 
   expression = createTokenizer("1");
-  token = getToken(expression);
-  intToken = (IntegerToken *)nudInt(token, expression,0);
+  token = getNextToken(expression);
+  token = nudInt(token, expression,0);
 
-  TEST_ASSERT_EQUAL (1,intToken->value);
+  TEST_ASSERT_EQUAL (1,((IntegerToken *)token)->value);
+  TEST_ASSERT_EQUAL (INTEGER_SYMBOL, token->symbol);
 }
 
-void test_getNextToken_given_expression_1_should_return_value_1(void) {
+void test_getNextToken_given_expression_1_should_return_token_with_symbol_1(void) {
   Tokenizer *expression;
   Token *token;
 
@@ -46,6 +46,25 @@ void test_getNextToken_given_expression_1_should_return_value_1(void) {
   token = getNextToken(expression);
 
   TEST_ASSERT_EQUAL (1,((IntegerToken *)token)->value);
+  TEST_ASSERT_EQUAL (INTEGER_SYMBOL, token->symbol);
+}
+
+void test_getNextToken_given_expression_23_plus_98_should_return_3_tokens_23_plus_98(void) {
+  Tokenizer *expression;
+  Token *v1Token, *v2Token, *opToken;
+
+  expression = createTokenizer(" 23 + 98 ");
+  v1Token = getNextToken(expression);
+  opToken = getNextToken(expression);
+  v2Token = getNextToken(expression);
+
+  TEST_ASSERT_EQUAL (23,((IntegerToken *)v1Token)->value);
+  TEST_ASSERT_EQUAL (INTEGER_SYMBOL, v1Token->symbol);
+  TEST_ASSERT_EQUAL (ADD_SYMBOL,opToken->symbol);
+  TEST_ASSERT_EQUAL_STRING  ("+", opToken->str);
+  TEST_ASSERT_EQUAL (98,((IntegerToken *)v2Token)->value);
+  TEST_ASSERT_EQUAL (INTEGER_SYMBOL, v2Token->symbol);
+
 }
 
 void test_verifyTokenBackToBack_given_double_closed_AND_should_return_true(void) {
@@ -59,6 +78,17 @@ void test_verifyTokenBackToBack_given_double_closed_AND_should_return_true(void)
   TEST_ASSERT_TRUE (verifyTokensBackToBack(token1,token2));
 }
 
+void test_verifyTokenBackToBack_given_double_separated_EQUALS_should_return_false(void) {
+  Tokenizer *expression;
+  Token *token1, *token2;
+
+  expression = createTokenizer(" =    = ");
+  token1 =getToken(expression);
+  token2 = getToken(expression);
+
+  TEST_ASSERT_FALSE (verifyTokensBackToBack(token1,token2));
+}
+
 void test_modifyToken_given_logical_AND_symbol_should_return_double_AND_str_in_token(void) {
   Tokenizer *expression;
   Token *token, *token1;
@@ -68,6 +98,7 @@ void test_modifyToken_given_logical_AND_symbol_should_return_double_AND_str_in_t
   token = modifyToken(token1, LOGICAL_AND_SYMBOL);
 
   TEST_ASSERT_EQUAL_STRING ("&&", token->str);
+  TEST_ASSERT_EQUAL(LOGICAL_AND_SYMBOL, token->symbol);
 }
 
 void test_getNextToken_given_NOT_EQUAL_expression_should_return_token_str_NOT_EQUAL(void) {
@@ -78,6 +109,7 @@ void test_getNextToken_given_NOT_EQUAL_expression_should_return_token_str_NOT_EQ
   token = getNextToken(expression);
 
   TEST_ASSERT_EQUAL_STRING ("!=", token->str);
+  TEST_ASSERT_EQUAL(NOT_EQUALS_TO_SYMBOL, token->symbol);
 }
 
 void test_getNextToken_given_separated_AND_expression_should_return_token_str_BITWISE_OR(void) {
@@ -88,6 +120,7 @@ void test_getNextToken_given_separated_AND_expression_should_return_token_str_BI
   token = getNextToken(expression);
 
   TEST_ASSERT_EQUAL_STRING ("&", token->str);
+  TEST_ASSERT_EQUAL(BITWISE_AND_SYMBOL, token->symbol);
 }
 
 void test_getNextToken_given_negative_2_expression_should_return_token_with_negative_symbol(void) {
